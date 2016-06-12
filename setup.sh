23701606fd52589a9e83f7b6eb116c142b30669b
Jaime Geiger <jaime@grimm-co.com>
2016-06-12 15:20:22 -0400

    setup

--- setup.sh
+++ setup.sh
@@ -9,6 +9,9 @@ fi
 
 git clone https://github.com/wumb0/dotfiles-new
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-cp dotfiles-new/.??* $HOME
+cp dotfiles-new/.??* ~
+git clone https://github.com/oskarkrawczyk/honukai-iterm-zsh
+cp honukai-iterm-zsh/honukai.zsh-theme ~/.oh-my-zsh/custom/themes
+rm -rf honukai-iterm-zsh dotfiles-new
 
 echo "Done, launch vim to install bundles, configure plugins for oh my zsh in .zshrc"
