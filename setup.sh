668784204a32a5f4df76fe3a6d2604f0d2eee985
Jaime Geiger <jaime@grimm-co.com>
2016-06-12 15:08:59 -0400

    setup

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+if [ ! -z `which zsh` ]; then
+    echo "Changing shell to zsh"
+    chsh -s $(which zsh)
+else
+    echo "Zsh not installed, do that first"
+    exit
+fi
+
+git clone https://github.com/wumb0/dotfiles-new
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+cp dotfiles-new/.??* $HOME
+
+echo "Done, launch vim to install bundles, configure plugins for oh my zsh in .zshrc"
